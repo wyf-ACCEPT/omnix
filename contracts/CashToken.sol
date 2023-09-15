579127122acd1523174cb1eb4a06a51377872ad6
wyf-ACCEPT <define_wifi@163.com>
2023-09-15 13:26:47 +0800

    init

---
+++ contracts/CashToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache License 2.0
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CashToken is ERC20 {
+    constructor () ERC20("Anonymous Omnix", "ZTH") {
+        // etc
+    }
+
+    function mint(address account, uint256 amount) external { // just for testing---expose the method.
+        _mint(account, amount);
+    }
+}
